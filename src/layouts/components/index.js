6ebbf39ba967c1237ec78baa3a7cbf833731f361
初志鑫 <1204505056@qq.com>
2020-05-23 16:12:37 +0800

    deploy

--- src/layouts/components/index.js
+++ src/layouts/components/index.js
@@ -1,5 +1,5 @@
-export { default as TopBar } from "./TopBar";
-export { default as NavBar } from "./NavBar";
-export { default as TagsBar } from "./TagsBar";
-export { default as SideBar } from "./SideBar";
 export { default as AppMain } from "./AppMain";
+export { default as NavBar } from "./NavBar";
+export { default as TopBar } from "zx-top-bar/index";
+export { default as TagsBar } from "zx-tags-bar/index";
+export { default as SideBar } from "zx-side-bar/index";
